d31661e9b5d1809c9757ab0c1766a19cf67661cf
David Young-Chan Kay <davidykay@gmail.com>
2012-09-12 23:17:41 -0700

    hello UDP from python to go, but go is crashing.

--- chat/chatserver.go
+++ chat/chatserver.go
@@ -1,58 +1,145 @@
 package main
 
 import (
-    "code.google.com/p/go.net/websocket"
+    //"code.google.com/p/go.net/websocket"
+    "bytes"
     "flag"
     "fmt" 
     "log"
     "net/http"
+    "net"
     "text/template"
 )
 
 ////////////////////////////////////////
-// Cht Connection
+// Chat Connection
 ////////////////////////////////////////
 
+const BUFFER_SIZE = 256
+
+//type wsconnection struct {
+//    // The websocket connection.
+//    socket *websocket.Conn
+//
+//    // Buffered channel of outbound messages.
+//    send chan string
+//}
+
+//type connection struct {
+//    // The websocket connection.
+//    socket *net.Conn
+//
+//    // Buffered channel of outbound messages.
+//    send chan string
+//}
+
+//type connection struct {
 type connection struct {
     // The websocket connection.
-    ws *websocket.Conn
+    socket *net.UDPConn
 
     // Buffered channel of outbound messages.
     send chan string
 }
 
-func (c *connection) reader() {
+//func (c *connection) wsReader() {
+//    for {
+//        var message string
+//        err := websocket.Message.Receive(c.socket, &message)
+//        if err != nil {
+//            break
+//        }
+//        // IF this is a chat message, broadcast it:
+//        h.broadcast <- message
+//        // TODO: IF this is a movement command, hand it to nav system
+//        // TODO: IF we don't recognize it, throw an error
+//    }
+//    c.socket.Close()
+//}
+
+func (c *connection) udpReader() {
     for {
         var message string
-        err := websocket.Message.Receive(c.ws, &message)
+
+        //func (c *UDPConn) ReadFromUDP(b []byte) (n int, addr *UDPAddr, err error)
+        
+        buf := make([]byte, 1024)
+        n, err := c.socket.Read(buf)
+
         if err != nil {
-            break
-        }
+            log.Fatalln("error reading UDP: ", err)
+            //break
+	}
+        message = string(buf[0:n])
+
         // IF this is a chat message, broadcast it:
         h.broadcast <- message
         // TODO: IF this is a movement command, hand it to nav system
         // TODO: IF we don't recognize it, throw an error
     }
-    c.ws.Close()
+    c.socket.Close()
 }
 
-func (c *connection) writer() {
+
+//func (c *connection) wsWriter() {
+//    for message := range c.send {
+//        fmt.Println("Sending message: " + message) 
+//        err := websocket.Message.Send(c.socket, message)
+//        if err != nil {
+//            break
+//        }
+//    }
+//    c.socket.Close()
+//}
+
+func (c *connection) udpWriter() {
     for message := range c.send {
         fmt.Println("Sending message: " + message) 
-        err := websocket.Message.Send(c.ws, message)
+
+        // TODO: convert to bytes
+        //buf := strings.Buffer.NewBufferString(message)
+        buf := bytes.NewBufferString(message).Bytes()
+
+        n, err := c.socket.Write(buf) 
+
         if err != nil {
-            break
-        }
+            log.Fatalln("error writing to UDP: ", err, n)
+            //log.Fatalln(n)
+            //log.Fatalln(err)
+            //break
+	}
+
     }
-    c.ws.Close()
+    c.socket.Close()
 }
 
-func wsHandler(ws *websocket.Conn) {
-    c := &connection{send: make(chan string, 256), ws: ws}
+
+//func handleRawSocket(socket net.Conn) {
+//    c := &connection{send: make(chan string, 256), socket: socket}
+//    h.register <- c
+//    defer func() { h.unregister <- c }()
+//    go c.writer()
+//    c.reader()
+//}
+
+//func wsHandler(ws *websocket.Conn) {
+//    c := &connection{send: make(chan string, BUFFER_SIZE), socket: ws}
+//    h.register <- c
+//    defer func() { h.unregister <- c }()
+//    go c.writer()
+//    c.wsReader()
+//}
+
+//func handleTcp(socket TCPConn) {
+//    handleRawSocket(socket)
+//}
+
+func handleUdp(socket *net.UDPConn) {
+    c := &connection{send: make(chan string, BUFFER_SIZE), socket: socket}
     h.register <- c
     defer func() { h.unregister <- c }()
-    go c.writer()
-    c.reader()
+    go c.udpWriter()
+    c.udpReader()
 }
 
 ////////////////////////////////////////
@@ -98,7 +185,7 @@ func (h *hub) run() {
                 default:
                     delete(h.connections, c)
                     close(c.send)
-                    go c.ws.Close()
+                    go c.socket.Close()
                 }
             }
         }
@@ -119,9 +206,46 @@ func homeHandler(c http.ResponseWriter, req *http.Request) {
 func main() {
     flag.Parse()
     go h.run()
-    http.HandleFunc("/", homeHandler)
-    http.Handle("/ws", websocket.Handler(wsHandler))
-    if err := http.ListenAndServe(*addr, nil); err != nil {
-        log.Fatal("ListenAndServe:", err)
+
+    //http.HandleFunc("/", homeHandler)
+    //http.Handle("/ws", websocket.Handler(wsHandler))
+    //if err := http.ListenAndServe(*addr, nil); err != nil {
+    //    log.Fatal("Websocket ListenAndServe:", err)
+    //}
+
+    //ln, err := net.Listen("tcp", ":5005")
+    //if err != nil {
+    //    log.Fatal("TCP Listen:", err)
+    //}
+    //for {
+    //    conn, err := ln.Accept()
+    //    if err != nil {
+    //        log.Error("TCP accept error:", err)
+    //        // handle error
+    //        continue
+    //    }
+    //    go handleTcp(conn)
+    //}
+
+    //ln, err := net.ListenUDP("udp", ":4004")
+    udpAddr := &net.UDPAddr{ IP: net.ParseIP("127.0.0.1"), Port: 4004 }
+    conn, err := net.ListenUDP("udp", udpAddr)
+    if err != nil {
+        log.Fatal("UDP Listen:", err)
+    } else {
+        fmt.Println("listening on ", conn.LocalAddr().String())
     }
+
+    handleUdp(conn)
+
+    //for {
+    //    conn, err := ln.Accept()
+    //    if err != nil {
+    //        log.Fatal("UDP accept error:", err)
+    //        // handle error
+    //        continue
+    //    }
+    //    go handleUdp(conn)
+    //}
+
 }
