6a96949de37e569713ae96ee00ca850ebfb6dfcf
David Young-Chan Kay <davidykay@gmail.com>
2012-09-14 21:06:15 -0400

    now no longer crashing on hello world. also working on main loop

--- server/chatserver.go
+++ server/chatserver.go
@@ -4,7 +4,7 @@ import (
     //"code.google.com/p/go.net/websocket"
     "bytes"
     "flag"
-    "fmt" 
+    "fmt"
     "log"
     "net"
 )
@@ -15,47 +15,48 @@ import (
 
 const BUFFER_SIZE = 256
 
-//type connection struct {
 type connection struct {
     // The websocket connection.
     socket *net.UDPConn
+
+    address *net.UDPAddr
 
     // Buffered channel of outbound messages.
     send chan string
 }
 
-func (c *connection) udpReader() {
+func listenUdp(c *net.UDPConn) {
+    //m := make(map[string]int, 100)
     for {
         var message string
 
-        //func (c *UDPConn) ReadFromUDP(b []byte) (n int, addr *UDPAddr, err error)
-        
         buf := make([]byte, 1024)
-        n, err := c.socket.Read(buf)
+        n, addr, err := c.ReadFromUDP(buf)
 
         if err != nil {
             log.Fatalln("error reading UDP: ", err)
             //break
 	}
         message = string(buf[0:n])
+        log.Println("received message from ", addr, message)
 
         // IF this is a chat message, broadcast it:
-        h.broadcast <- message
+        //h.broadcast <- message
         // TODO: IF this is a movement command, hand it to nav system
         // TODO: IF we don't recognize it, throw an error
     }
-    c.socket.Close()
+    c.Close()
 }
 
 func (c *connection) udpWriter() {
     for message := range c.send {
-        fmt.Println("Sending message: " + message) 
+        fmt.Println("Sending message: " + message)
 
         // TODO: convert to bytes
         //buf := strings.Buffer.NewBufferString(message)
         buf := bytes.NewBufferString(message).Bytes()
 
-        n, err := c.socket.Write(buf) 
+        n, err := c.socket.Write(buf)
 
         if err != nil {
             log.Fatalln("error writing to UDP: ", err, n)
@@ -68,13 +69,13 @@ func (c *connection) udpWriter() {
     c.socket.Close()
 }
 
-func handleUdp(socket *net.UDPConn) {
-    c := &connection{send: make(chan string, BUFFER_SIZE), socket: socket}
-    h.register <- c
-    defer func() { h.unregister <- c }()
-    go c.udpWriter()
-    c.udpReader()
-}
+//func listenUdp(socket *net.UDPConn) {
+//    c := &connection{send: make(chan string, BUFFER_SIZE), socket: socket}
+//    h.register <- c
+//    defer func() { h.unregister <- c }()
+//    go c.udpWriter()
+//    c.udpReader()
+//}
 
 ////////////////////////////////////////
 // Chat Hub
@@ -98,7 +99,7 @@ var h = hub{
     broadcast: make(chan string),
     register: make(chan *connection),
     unregister: make(chan *connection),
-    connections: make(map[*connection]net.UDPAddr),
+    connections: make(map[*connection]bool),
 }
 
 func (h *hub) run() {
@@ -120,19 +121,17 @@ func (h *hub) run() {
                     delete(h.connections, c)
                     close(c.send)
                     go c.socket.Close()
-                }
+                 }
             }
         }
     }
 }
-
 ////////////////////////////////////////
 // Main
 ////////////////////////////////////////
 
 func main() {
     flag.Parse()
-    m := make(map[string]int, 100)
     go h.run()
 
     udpAddr := &net.UDPAddr{ IP: net.ParseIP("127.0.0.1"), Port: 4004 }
@@ -143,7 +142,7 @@ func main() {
         fmt.Println("listening on ", conn.LocalAddr().String())
     }
 
-    handleUdp(conn)
+    listenUdp(conn)
 
     //for {
     //    conn, err := ln.Accept()
